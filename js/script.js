1f789d27784bc9f941d47e836b40d98c04681bec
SokolowaEkaterina <soko20wa.ek0ter1na@gmail.com>
2020-08-21 23:57:05 +0400

    p

---
+++ js/script.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    const menuToggle = document.querySelector('#menu_togle');
+    const mobileNavContainer = document.querySelector('#mobile-nav');
+
+
+    menuToggle.onclick = function(){
+    menuToggle.classList.toggle('menu-icon-active');
+    mobileNavContainer.classList.toggle('mobile-nav--active'); 
+}
+});
